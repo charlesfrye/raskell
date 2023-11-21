e8aef145d40642b39c319dd98b078ad5a2274ef6
Charles Frye <cfrye59@gmail.com>
2023-11-21 10:04:15 -0800

    better sorting demonstration

--- app/Sort.hs
+++ app/Sort.hs
@@ -1,12 +1,33 @@
-module Sort where
+module Sort (main) where
 
 import Core
-import Lib
+import Lib (gt, sample)
 
+-- | Θ(n^2) time, Θ(n) space sorting algorithm
 sort :: Sequence -> Sequence
-sort xs = decode endOfSequence raspSort (xs ++ [minBound]) (fromIntegral (length xs))
+sort xs = sample endOfSequence raspSort (prep xs) seqLength
   where
-    endOfSequence = maximum xs
+    endOfSequence = maxBound
+    seqLength = fromIntegral (length xs)
     raspSort :: Sequence -> Sequence
-    -- Sort by looking for the smallest element greater than the current token
+    -- Sort by looking for the smallest token greater than the current token
     raspSort s = minKQV s s gt s
+    prep :: Sequence -> Sequence
+    -- Prepare a sequence for sorting by padding with max/min values
+    prep s = [maxBound] ++ s ++ [minBound]
+
+-- | Sorts a newline-separated collection of space-separated int8 sequences
+main :: IO ()
+main = do
+  contents <- getContents
+  let sorts = map sort lists
+      lists = map parse $ lines contents
+  mapM_ printClean sorts
+
+parse :: String -> [Token]
+parse = map read . words
+
+printClean :: Sequence -> IO ()
+printClean = putStrLn . unwords . map show . dropPrefix
+  where
+    dropPrefix = drop 1 . dropWhile (> minBound)
